e029beb7faa46ef749a43bbca1b26e04bae2181c
cg-alves <5had0w5talk3r@disroot.org>
2018-06-14 17:31:34 +0100

    Update and rename pph-plugin-search.php to cgaph-plugin-search.php

--- src/plugin-helper/cgaph-plugin-search.php
+++ src/plugin-helper/cgaph-plugin-search.php
@@ -2,40 +2,40 @@
 
 // This function sets up manual searching of plugins.
 // You can use it to install several plugins at once.
-function pph_plugin_searcher() {
+function cgaph_plugin_searcher() {
 
     if ( isset ( $_POST['search'] ) ) {
         $tmp = stripslashes ( $_POST['search'] );
         $search = ( explode ( PHP_EOL, $tmp ) );
-        pph_plugin_search( $search );
+        cgaph_plugin_search( $search );
     }
     
     ?>
         <div class="wrap">
-            <h1 class="wp-heading-inline"><?php echo esc_attr( __( "Plugin Search", 'pragmatica-plugin-helper' ) ); ?></h1>
-            <a class='page-title-action' href='../../wp-admin/admin.php?page=plugin-installer'><?php echo esc_attr( __( "Return to the Plugin's Homepage", 'pragmatica-plugin-helper' ) ); ?></a>
+            <h1 class="wp-heading-inline"><?php echo esc_attr( __( "Plugin Search", 'cga-plugin-helper' ) ); ?></h1>
+            <a class='page-title-action' href='../../wp-admin/admin.php?page=plugin-installer'><?php echo esc_attr( __( "Return to the Plugin's Homepage", 'cga-plugin-helper' ) ); ?></a>
             <hr class='wp-header-end'>
             <table class='wp-list-table widefat striped tags'>
                 <caption>
-                    <h3><?php echo esc_attr( __( "Please insert the name of the plugins you wish to install in the format bellow.", 'pragmatica-plugin-helper' ) ); ?></h3>
+                    <h3><?php echo esc_attr( __( "Please insert the name of the plugins you wish to install in the format bellow.", 'cga-plugin-helper' ) ); ?></h3>
                 </caption>
                 <thead>
                     <tr>
-                        <th class='manage-column column-author' colspan='2' style='text-align:center;'><?php echo esc_attr( __( "Plugins to Install", 'pragmatica-plugin-helper' ) ); ?></th>
+                        <th class='manage-column column-author' colspan='2' style='text-align:center;'><?php echo esc_attr( __( "Plugins to Install", 'cga-plugin-helper' ) ); ?></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td class='manage-column column-author' colspan='2' style='text-align:center;'>
                         <form method='post' name='search'>
-                            <textarea name='search' class='widefat' style='width:100% !important; height:300px !important; overflow:auto;' placeholder='<?php echo esc_attr( __( 'Insert the name of the plugins that you wish to install separated by either apostrophes or quotation marks and Return.&#10;&#10;Example:&#10;"yoast"&#10;"beaver builder"&#10;"tinymce"', 'pragmatica-plugin-helper' ) ); ?>'></textarea>
+                            <textarea name='search' class='widefat' style='width:100% !important; height:300px !important; overflow:auto;' placeholder='<?php echo esc_attr( __( 'Insert the name of the plugins that you wish to install separated by either apostrophes or quotation marks and Return.&#10;&#10;Example:&#10;"yoast"&#10;"beaver builder"&#10;"tinymce"', 'cga-plugin-helper' ) ); ?>'></textarea>
                         </td>
                     </tr>
                 </tbody> 
                 <tfoot>
                     <tr>
                         <td class='manage-column column-author' colspan='2' style='text-align:center;'>
-                            <input class='button button-primary button-hero load-customize hide-if-no-customize' type='submit' name='plugin_select' value='<?php echo esc_attr( __( 'Install Plugins', 'pragmatica-plugin-helper' ) ); ?>'/>
+                            <input class='button button-primary button-hero load-customize hide-if-no-customize' type='submit' name='plugin_select' value='<?php echo esc_attr( __( 'Install Plugins', 'cga-plugin-helper' ) ); ?>'/>
                         </td>
                     </tr>
                 </form>
@@ -46,7 +46,7 @@ function pph_plugin_searcher() {
 }
 
 //This function will search for the plugins in the WP store, save the result in an array, and store it to a transient so it can later be used by the installer function.
-function pph_plugin_search( $search ) {
+function cgaph_plugin_search( $search ) {
     include ( './includes/plugin-install.php' );
     $i = 1;
     
