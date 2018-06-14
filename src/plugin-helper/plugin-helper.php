4c125bc394d8a6db01d7503decb2d5df2172bcc1
cg-alves <5had0w5talk3r@disroot.org>
2018-06-14 14:25:55 +0100

    Update plugin-helper.php

--- src/plugin-helper/plugin-helper.php
+++ src/plugin-helper/plugin-helper.php
@@ -34,6 +34,7 @@ function pph_load_textdomain() {
 //Loads menu and pages
 function pph_installer_menu() {
     include_once ( 'pph-landing-page.php' );
+    include_once ( 'pph-plugin-select.php' );
     include_once ( 'pph-plugin-search.php' );
     include_once ( 'pph-plugin-export.php' );
     include_once ( 'pph-plugin-main.php' );
@@ -53,55 +54,4 @@ function pph_session_starter(){
 
 function pph_session_ender(){
     session_destroy ();
-}
-
-// A curated plugin list.
-// You can provide your clients with a custom .php file containing a modified array in the style of the one included in the "else" bellow. This must then be added to obvious path listed in the "if" statement. Future versions of this plugin will likely include additional categories, so be sure to read the patch notes when updating.
-// If you decide to add any categories in the mean time, be sure to adjust for that in "function plugin_form ( $plugins )".
-function pph_curated_plugin_list() {
-    $uploads_dir = wp_upload_dir();
-    $pph_curated_plugins_dir = ( $uploads_dir['basedir'].'/pragmatica-plugin-helper' );
-    
-    //Checks if the required upload directory exists, and creates it if it doesn't.
-    if ( !file_exists ( $pph_curated_plugins_dir ) && !is_dir( $pph_curated_plugins_dir ) ) {
-        mkdir ( $pph_curated_plugins_dir );
-    } 
-    
-    if ( file_exists ( $pph_curated_plugins_dir.'/custom-curated-plugin-list.php' ) ) {
-        include ( $pph_curated_plugins_dir.'/custom-curated-plugin-list.php' );
-    }
-        
-    else{
-        include ( 'pph-curated-plugin-list.php' );
-    }
-    return ( $curated_plugins );
-}
-
-//This function allows the end user to select the plugins they wish to install.
-//It takes the form data from $_POST and will store it in $_SESSION if the user ticked the selection box.
-function pph_plugin_selector() {
-    $plugins = pph_curated_plugin_list();
-    if ( isset ( $_POST['plugin_select'] ) ) {
-        foreach ( $plugins as $each_plugin => $plugin_field ) {
-            if ( $_POST[$plugin_field['id']] == '1' ) {
-                $plugins[$plugin_field['id']] [ 'apply' ] = '1' ;
-                }
-            else {
-                $plugins[$plugin_field['id']] [ 'apply' ] = '0' ;
-            }
-        }
-        $_SESSION['plugin'] = $plugins;
-		echo '<script>window.location.href="admin.php?page=plugin-installer-main"</script>'; //Workaround for redirecting within the wp-admin page.
-    }
-    ?>
-    <div class='wrap'>
-            <h1 class='wp-heading-inline'><?php echo esc_attr( __( 'Curated Plugin List', 'pragmatica-plugin-helper' ) ); ?></h1>
-            <a class='page-title-action' href='../../wp-admin/admin.php?page=plugin-installer'><?php echo esc_attr( __( "Return to the Plugin's Homepage", 'pragmatica-plugin-helper' ) ); ?></a>
-            <hr class='wp-header-end'>
-    <?php
-        include_once ('pph-plugin-form.php');
-        pph_plugin_form( $plugins );
-    ?>
-        </div>
-    <?php
 }
